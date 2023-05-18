bdbc0fd81742696474f7363c45effbe2d9ca3435
Stephan <cicchelli.stephan@gmail.com>
2023-05-18 17:05:03 -0300

    Req 6&7

--- script.js
+++ script.js
@@ -15,8 +15,26 @@ document.body.appendChild(generateRandomColorsButton);
 
 generateRandomColorsButton.addEventListener('click', () => {
   console.log(generateRandomColor());
+  const ramdomColor = [];
   for (let index = 1; index < colorBoxes.length; index += 1) {
     const ramdom = generateRandomColor();
     colorBoxes[index].style.backgroundColor = ramdom;
+    ramdomColor.push(ramdom);
   }
+  return ramdomColor;
 });
+
+const square = () => {
+  for (let i = 0; i < 5; i += 1) {
+    const createBoard = document.createElement('body');
+    createBoard.setAttribute('id', 'pixel-board');
+    document.body.appendChild(createBoard);
+    for (index = 0; index < 5; index += 1) {
+      const createPixels = document.createElement('body');
+      createPixels.classList.add('pixel');
+      createBoard.appendChild(createPixels);
+      createPixels.style.backgroundColor = 'white';
+    }
+  }
+};
+square();
